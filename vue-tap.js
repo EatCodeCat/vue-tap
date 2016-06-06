974955204435f4cb95be9e7aaba8561a3cd32235
linjunfeng <linjunfeng@kankan.com>
2016-06-06 16:35:07 +0800

    remove the time limit

--- vue-tap.js
+++ vue-tap.js
@@ -12,37 +12,69 @@
                 var self = this;
 
                 self.handler = function() {};
-                self.tapInfo = {};
+                self.tapInfo = {
+                    x1: null,
+                    y1: null,
+                    x2: null,
+                    y2: null
+                };
                 self.touchstartFn = function(e) {
                     if(self.modifiers.stopPropagation) e.stopPropagation();
                     if(self.modifiers.preventDefault) e.preventDefault();
                     self._touchstart(self, e);
                 };
+
+                self.touchmoveFn = function (e) {
+                    self._touchmove(self, e);
+                };
+
                 self.touchendFn = function(e) {
                     self._touchend(self, e);
                 };
 
-                this.el.addEventListener('touchstart', self.touchstartFn);
+                self.touchcancelFn = function(e) {
+                    self._touchcancel(self, e);
+                };
 
-                this.el.addEventListener('touchend', self.touchendFn);
+                this.el.addEventListener('touchstart', self.touchstartFn.bind(this), false);
+                this.el.addEventListener('touchmove', self.touchmoveFn.bind(this), false);
+                this.el.addEventListener('touchend', self.touchendFn.bind(this), false);
+                this.el.addEventListener('touchcancel', self.touchcancelFn.bind(this), false);
             },
 
-            _touchstart: function(obj, event) {
-                var touch = event.touches[0];
-                obj.tapInfo.pageX = touch.pageX;
-                obj.tapInfo.pageY = touch.pageY;
-                obj.tapInfo.touchStartTime = (new Date()).getTime();
+            _touchstart: function(obj, evt) {
+                if(!evt.touches)return;
+
+                var touch = evt.touches[0];
+                obj.tapInfo.x1 = touch.pageX;
+                obj.tapInfo.y1 = touch.pageY;
             },
 
-            _touchend: function(obj, event) {
-                var touch = event.changedTouches[0];
-                var interval = (new Date()).getTime() - obj.tapInfo.touchStartTime;
-                var disX = touch.pageX - obj.tapInfo.pageX;
-                var disY = touch.pageY - obj.tapInfo.pageY;
+            _touchmove: function (obj, evt) {
+                if(!evt.touches)return;
 
-                if (interval < 100 && Math.abs(disX) < 2 && Math.abs(disY) < 2) {
-                    obj.handler(event);
+                var touch = evt.touches[0];
+                obj.tapInfo.x2 = touch.pageX;
+                obj.tapInfo.y2 = touch.pageY;
+            },
+
+            _touchend: function(obj, evt) {
+                if(!evt.changedTouches)return;
+
+                if ((obj.tapInfo.x2 && Math.abx(obj.tapInfo.x1 - obj.tapInfo.x2) > 30) || 
+                    (obj.tapInfo.y2 && Math.abx(obj.tapInfo.y1 - obj.tapInfo.y2) > 30)) {
+                    // swipe
+                } else {
+                    obj.tapTimeout = setTimeout(function () {
+                        obj.handler(evt);
+                    }, 0);
                 }
+
+                obj.tapInfo.x1 = obj.tapInfo.y1 = obj.tapInfo.x2 = obj.tapInfo.y2 = null;
+            },
+
+            _touchcancel: function (obj, evt) {
+                clearTimeout(obj.tapTimeout);
             },
 
             update: function(fn) {
@@ -53,8 +85,10 @@
 
             unbind: function() {
                 this.el.removeEventListener('touchstart', this.touchstartFn);
+                this.el.removeEventListener('touchmove', this.touchmoveFn);
                 this.el.removeEventListener('touchend', this.touchendFn);
-                this.touchstartFn = this.touchendFn = null;
+                this.el.removeEventListener('touchcancel', this.touchcancelFn);
+                this.touchstartFn = this.touchmoveFn = this.touchendFn = this.touchcancelFn = null;
                 this.handler = this.tapInfo = null;
             }
         });
